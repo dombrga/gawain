d326a1b5e5ec2f101e7bb211b7d07944ec6929d9
dombrga <dom.barga@gmail.com>
2024-04-18 00:39:52 +0800

    app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IonApp, IonButton, IonCol, IonContent, IonGrid, IonRow } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    IonApp, 
+    IonButton,
+    IonContent,
+    IonGrid, IonRow, IonCol,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gawain-angular';
+  
+  constructor(
+    
+  ) {
+    
+  }
 }
