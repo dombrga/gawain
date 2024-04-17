703597104ab1defb0998167746b2884145fa77a5
dombrga <dom.barga@gmail.com>
2024-04-18 00:40:03 +0800

    added 404 route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register/register.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent, title: 'register' },
   { path: 'login', component: LoginComponent, title: 'login' },
-  // { path: 'login2', component: LoginComponent2, title: 'home' }
+  // { path: 'login2', component: LoginComponent2, title: 'home' },
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
